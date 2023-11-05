39fbb7c5abd9c1009f86f6f24aeeab04c14a6916
Enes Kaya <eng.eneskaya@gmail.com>
2023-11-05 22:41:50 +0300

    Initial commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Enes Kaya on 5.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
